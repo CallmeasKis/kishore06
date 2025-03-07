db3076ef21fa0d4e1a6fced81a2c468df918bc53
Kishore <94296387+CallmeasKis@users.noreply.github.com>
2025-03-07 09:43:34 +0530

    Update Freele.java

--- day4/Freele.java
+++ day4/Freele.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class FrequentElement {
+ class FrequentElement {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
