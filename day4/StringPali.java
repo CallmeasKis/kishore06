9865897255e1f96c9596220c5983d5c0b8bf3004
Kishore <94296387+CallmeasKis@users.noreply.github.com>
2025-03-07 09:44:20 +0530

    Update StringPali.java

--- day4/StringPali.java
+++ day4/StringPali.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringPalindrome {
+ class StringPalindrome {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         String s=scan.next();
