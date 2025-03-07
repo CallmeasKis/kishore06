a61d21afe84ce11dbfa2fa5a3db8f650d009e18c
Kishore <94296387+CallmeasKis@users.noreply.github.com>
2025-03-07 09:42:57 +0530

    Update Findlnstr.java

--- day4/Findlnstr.java
+++ day4/Findlnstr.java
@@ -1,7 +1,6 @@
 
 import java.util.Scanner;
-
-public class FindLongestInString {
+ class FindLongestInString {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         String str=scan.nextLine();
